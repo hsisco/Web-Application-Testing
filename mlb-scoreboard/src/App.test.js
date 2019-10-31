b53c29cda6ab1744e5b0bcbbe6fe822a211ff392
hsisco <hysensisco@gmail.com>
2019-10-31 15:36:26 -0700

    Add tests to check for MVP

--- mlb-scoreboard/src/App.test.js
+++ mlb-scoreboard/src/App.test.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Display, { addStrike, addBall, addFoul } from './components/Display';
+
 
 test('✅renders without crashing', () => {
   render(<App />)
+});
+
+test('✅ Strikes reset at 3', () => {
+  expect(addStrike(3)).toBe(0);
+});
+test('✅ Balls reset at 3', () => {
+  expect(addBall(4)).toBe(0);
+});
+test('✅ Balls + Strikes reset with Hit', () => {
+  expect(hit()).toBe(0);
+});
+test('✅ Foul increases Strikes up to 2', () => {
+  expect(addFoul(2)).toBe(2);
 });
