1b66b872fa8b52a9a8e2dc653494fa9085f2e069
hsisco <hysensisco@gmail.com>
2019-10-31 13:40:59 -0700

    Set up Display class component

---
+++ mlb-score-board/src/components/Display.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Dashboard from './Dashboard';
+
+export class Display extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      strikes: 0,
+      balls: 0
+    }
+  };
+
+  
+}
