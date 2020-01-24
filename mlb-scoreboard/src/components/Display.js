74813358b8653355de455cfa25af49a543a75b74
hsisco <hysensisco@gmail.com>
2020-01-23 19:37:56 -0800

    Display state

--- mlb-scoreboard/src/components/Display.js
+++ mlb-scoreboard/src/components/Display.js
@@ -3,5 +3,11 @@ import { Dashboard } from './Dashboard';
 
 
 export class Display extends React.Component {
-
+  constructor(){
+    super();
+    this.state = {
+      strikes: 0,
+      balls: 0
+    }
+  };
 }
