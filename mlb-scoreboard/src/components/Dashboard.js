49f497021c0588ddc6ccc80cbf0f98b400b55940
hsisco <hysensisco@gmail.com>
2020-01-23 19:40:58 -0800

    Build Display

--- mlb-scoreboard/src/components/Dashboard.js
+++ mlb-scoreboard/src/components/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Dashboard = (props) => {
-  return (
+  // return (
 
-  )
+  // )
 }
--- mlb-scoreboard/src/components/Display.js
+++ mlb-scoreboard/src/components/Display.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import { Dashboard } from './Dashboard';
 
 
@@ -10,4 +11,25 @@ export class Display extends React.Component {
       balls: 0
     }
   };
+
+  render(){
+    return(
+      <div className="Display">
+        <Container>
+          <Row>
+          <Col sm="4" />
+          <Col sm="4">
+            <h3>STRIKE</h3>
+            <h1>{this.state.strikes}</h1>
+            </Col>
+          <Col sm="4">
+            <h3>BALL</h3>
+            <h1>{this.state.balls}</h1>
+          </Col>
+          <Col sm="4" />
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
